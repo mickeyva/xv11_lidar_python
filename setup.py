1d44edc9c50bdccb16bce70d4a1c19ed7e2582cf
mickeyva <mikivardi@gmail.com>
2026-02-03 16:21:57 +0200

    launch 2

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'xv11_lidar_python'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/xv11_lidar_launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
