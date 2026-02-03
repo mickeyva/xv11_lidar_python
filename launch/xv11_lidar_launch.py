2364f544c039c16c851e64b51515a11316056fac
mickeyva <mikivardi@gmail.com>
2026-02-03 16:37:21 +0200

    sdfsdf

--- launch/xv11_lidar_launch.py
+++ launch/xv11_lidar_launch.py
@@ -1,7 +1,37 @@
-<launch>
-  <node name="xv11_lidar"          pkg="xv11_lidar_python"  type="xv11_lidar" output="screen">
-  <param name="serial_port"         type="string" value="/dev/ttyACM0"/>
-  <param name="serial_baudrate"     type="int"    value="115200"/>
-  <param name="frame_id"            type="string" value="laser"/>
-  </node>
-</launch>
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    port = LaunchConfiguration('port', default='/dev/ttyACM0')
+    frame_id = LaunchConfiguration('frame_id', default='laser')
+    
+    return LaunchDescription([
+
+        
+        DeclareLaunchArgument(
+            'port',
+            default_value=port,
+            description='Specifying usb port to connected lidar'),
+        
+        DeclareLaunchArgument(
+            'frame_id',
+            default_value=frame_id,
+            description='Specifying frame_id of lidar'),
+
+        Node(
+            package='xv11_lidar_python',
+            executable='xv11_lidar',
+            name='xv11_lidar',
+            parameters=[{'port': port,
+                         'frame_id': frame_id}],
+            output='screen'),
+    ])
